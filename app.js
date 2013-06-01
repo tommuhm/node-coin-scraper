e4449d607409c7cebcd15cda98c036090f1e0959
Tom Muhm <tom.muhm@gmail.com>
2013-06-01 12:33:12 +0200

    added some features for the btce module through a ext object: added currency pairs added http agent support

--- app.js
+++
@@ -1,2 +0,0 @@
-var btce = require("btce");
-
---
+++ btce_currencies.js
@@ -0,0 +1,13 @@
+module.exports = {
+    BTC: "BTC",
+    LTC: "LTC",
+    USD: "USD",
+    EUR: "EUR",
+    RUR: "RUR",
+    NMC: "NMC",
+    NVC: "NVC",
+    TRC: "TRC",
+    PPC: "PPC",
+    FTC: "FTC",
+    CNC: "CNC"
+}
